75e66725d07542045a5ab22becdea37e621b82b1
Ayaz <ATO.966000@gmail.com>
2024-04-24 20:23:25 +0300

    commit by Ayaz

--- lib/admin_screens/account_disabled_screen.dart
+++ lib/admin_screens/account_disabled_screen.dart
@@ -1,14 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
-class AccountDisabledScreen extends StatelessWidget {
+import '../components/actions.dart';
+import '../db/references.dart';
+import '../sr_screens/login_screen.dart';
+import '../widgets/admin_widgets/common_widgets/myBtn1.dart';
+
+class AccountDisabledScreen extends StatefulWidget {
   const AccountDisabledScreen({super.key});
 
   @override
+  State<AccountDisabledScreen> createState() => _AccountDisabledScreenState();
+}
+
+class _AccountDisabledScreenState extends State<AccountDisabledScreen> {
+  @override
+
+  logout(){
+    Fire.auth.signOut().then((value){
+      setState(() {
+        goToScreenAndClearHistory(context, const LoginScreen());
+      });
+
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
             child: Center(
-      child: Text("Your Account is disabled, Please Contact Customer Support"),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+          const CircleAvatar(
+              backgroundColor: Colors.transparent,
+              radius: 35,
+
+              child: Icon(Icons.warning, size: 65, color: Colors.redAccent)
+          ),
+          Gap(10),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Your Account is disabled, Please Contact Customer Support", textAlign: TextAlign.center, style:
+            TextStyle(
+              color: Colors.black45,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+
+            ),),
+          ),
+          Gap(10),
+          MyBtn1(buttonText: "Go Back",
+            onTap: (){
+              logout();
+            },),
+
+
+
+        ],
+      ),
     )));
+
+
   }
 }
--- lib/admin_screens/articles_screen.dart
+++ lib/admin_screens/articles_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -28,8 +30,19 @@ class _ArticlesPageState extends State<ArticlesPage> {
 
   Future<QuerySnapshot> getArticleInfo() async{
     articleInfo = await FirebaseChatServices().getArticlesDetails();
-
+  print("This article trigger");
     return articleInfo;
+  }
+  final storage = FirebaseStorage.instance;
+  String imgUrl = "";
+  Future<String> getImageUrl(String imageName) async {
+    final ref = storage.ref().child("BackgroundImages/"+imageName);
+    final url = await ref.getDownloadURL();
+    if (url is String) {
+      imgUrl = url;
+    }
+    print("This getImageUrl trigger");
+    return imgUrl;
   }
 
   Widget build(BuildContext context) {
@@ -40,21 +53,39 @@ class _ArticlesPageState extends State<ArticlesPage> {
           shrinkWrap: true,
           children: [
             Container(
+              padding: EdgeInsets.only(left: 10),
               alignment: Alignment.bottomRight,
               width: double.infinity,
               height: 100.0,
-              child: const SizedBox(
+              child: SizedBox(
                 height: 80,
-                child:  Image(
-                  image: AssetImage('assets/images/ic_logo.jpg'),
-                  fit: BoxFit.contain,
+                child:  Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        // Navigator.pushReplacement(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (context) => ProfileScreen()));
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        Icons.arrow_back,
+                      ),
+                    ),
+                    Image(
+                      image: AssetImage('assets/images/ic_logo.jpg'),
+                      fit: BoxFit.contain,
+                    ),
+                  ],
                 ),
               ),
             ),
             Gap(5),
             Padding(
               padding: const EdgeInsets.fromLTRB(25,5,5,0),
-              child: Text('Manage Accounts', style: kLabelEduMaterialsH_font,),
+              child: Text('Articles', style: kLabelEduMaterialsH_font,),
             ),
             Gap(10),
             FutureBuilder<QuerySnapshot>(
@@ -72,14 +103,63 @@ class _ArticlesPageState extends State<ArticlesPage> {
                       color: kQuoteBackgroundColor,
                       borderRadius: BorderRadius.circular(10),
                     ),
-                    margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    margin: EdgeInsets.fromLTRB(20, 15, 20, 10),
                     padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
                     child: Column(
-                      
+
                       children: [
-                        Text("Title : ${data.docs[0]["title"]}"),
-                        Gap(10),
-                        Text("Content : ${data.docs[0]["content"]}")
+                        FutureBuilder<String>(
+                            future: getImageUrl("articles.jpeg"),
+                            builder:
+                                (BuildContext context, AsyncSnapshot<String> snapshot) {
+                              if (snapshot.hasError)
+                                return Text("ERROR: ${snapshot.error}");
+                              if (!snapshot.hasData)
+                                return Center(child: CircularProgressIndicator());
+
+                              var data = snapshot.data;
+                              return Container(
+                                  width: 250,
+                                  height: 150,
+                                  child: Image.network(data!,fit: BoxFit.fill,
+
+                                    loadingBuilder: (BuildContext context, Widget child,
+                                        ImageChunkEvent? loadingProgress) {
+                                      if (loadingProgress == null) return child;
+                                      return Center(
+                                        child: CircularProgressIndicator(
+                                          value: loadingProgress.expectedTotalBytes != null
+                                              ? loadingProgress.cumulativeBytesLoaded /
+                                              loadingProgress.expectedTotalBytes!
+                                              : null,
+                                        ),
+                                      );
+                                    },
+
+                                    // decoration: BoxDecoration(
+                                    //   borderRadius: BorderRadius.circular(2),
+                                    //   image: DecorationImage(
+                                    //     image: AssetImage("assets/images/$url_img.jpeg"),
+                                    //     fit: BoxFit.cover,
+                                    //   ),
+                                    // ),
+                                  )
+
+                              );
+                            }),
+                        Gap(20),
+                        RichText(text: TextSpan(children: <TextSpan>[
+
+                          TextSpan(text: "Title :",style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
+                          TextSpan(text: " ${data.docs[0]["title"]}",style: const TextStyle( color: Colors.black))
+                        ])),
+                        Gap(20),
+                        RichText(text: TextSpan(children: <TextSpan>[
+
+                          TextSpan(text: "Content :",style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
+                          TextSpan(text: " ${data.docs[0]["content"]}",style: const TextStyle( color: Colors.black))
+                        ])),
+                        Gap(20),
                       ],
                     ),
                   );
